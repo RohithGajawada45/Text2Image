7ca21db2bba4d587acdbfa6b706f547550610c06
RohithGajawada <21b81a3336@cvr.ac.in>
2024-01-21 22:41:49 +0530

    NavBar almsot

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,9 +1,18 @@
-import './App.css'
+import React from 'react';
+import './App.css'; // Make sure your CSS file path is correct
+import Social from './Social';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <nav className="bg-transparent">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a className='text-white' href="http://www.instagram.com/codewithbiki" target='_blank' rel="noopener noreferrer">
+            <span className="self-center text-sm font-semibold whitespace-nowrap">Text2Image</span>
+          </a>
+           <Social />
+        </div>
+      </nav>
+    </div>
+  );
 }
